311163a6bb33398a04b67db643f92f863109d349
James Dickson <james_dickson1@baylor.edu>
2022-03-02 13:49:19 -0600

    commit

--- venvprac.py
+++ venvprac.py
@@ -1,2 +1,8 @@
 
-# Step 1: py -m -3 
+# Step 1: py -m -3 venv .venv
+
+# Step 2: .\.venv\Scripts\activate
+
+# Step 3: pip install matplotlib
+
+# Step 4: create .gitignore file and enter ignored file (.venv)
